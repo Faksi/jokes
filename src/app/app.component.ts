4071a71c5a66418f0fcd359b9fc44197f7fe6dc6
Faksi Jaladara <faksithecode89@gmail.cim>
2019-01-17 13:46:18 +0800

    last commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})  
+export class AppComponent {
+  title = 'app';
+
+  constructor(updates : SwUpdate, private data: DataService){
+  	updates.available.subscribe(event => {
+  		updates.activateUpdate().then(() => document.location.reload());
+  	})
+  }
+
+  joke: any;
+  ngOnInit() {
+  	this.data.gimmeJokes().subscribe(res => {
+  		this.joke = res;
+  	})
+  }
+}
